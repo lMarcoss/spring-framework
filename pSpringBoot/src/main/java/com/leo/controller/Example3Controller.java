b45f2f2792928265cf1241f16b3664d70718cba2
Leo <markos_193@hotmail.com>
2017-09-16 18:46:58 -0500

    página de error 404 personalizada

--- pSpringBoot/src/main/java/com/leo/controller/Example3Controller.java
+++ pSpringBoot/src/main/java/com/leo/controller/Example3Controller.java
@@ -1,5 +1,7 @@
 package com.leo.controller;
 
+import java.lang.ProcessBuilder.Redirect;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.leo.model.Person;
 
@@ -16,12 +19,26 @@ public class Example3Controller {
 	public static final String FORM_VIEW = "form";
 	public static final String RESULT_VIEW = "result";
 
+//	// Redirección 1
+//	@GetMapping("/")
+//	public String redirect() {
+//		return "redirect:/example3/showform";
+//	}
+
+	// Redireccionar con RedirectView
+	@GetMapping("/")
+	public RedirectView Redirect() {
+		return new RedirectView("/example3/showform");
+	}
+
+	// Muestra un formulario al escribir localhost:8080/example3/showform
 	@GetMapping("/showform")
 	public String showForm(Model model) {
 		model.addAttribute("person", new Person());
 		return FORM_VIEW;
 	}
 
+	// regresa la página con el objeto persona
 	@PostMapping("/addPerson")
 	public ModelAndView addPerson(@ModelAttribute("person") Person person) {
 		ModelAndView modelAndView = new ModelAndView(RESULT_VIEW);
---
+++ pSpringBoot/src/main/java/com/leo/controller/Example4Controller.java
@@ -0,0 +1,23 @@
+package com.leo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/example4")
+public class Example4Controller {
+	private static final String P404 = "404";
+	private static final String P500 = "500";
+
+	// Devuelve una página 404.html
+	@GetMapping("/404")
+	public String p404() {
+		return P404;
+	}
+
+	@GetMapping("/500")
+	public String p500() {
+		return P500;
+	}
+}
